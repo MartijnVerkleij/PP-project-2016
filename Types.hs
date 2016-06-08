3d36cf99fa7f0085307551696436e49c9b3f6280
Martijn Verkleij <github@mverkleij.nl>
2016-06-08 16:03:29 +0200

    added alphabet data type

--- Types.hs
+++ Types.hs
@@ -1,3 +1,27 @@
 module Types where
 
-
+data Alphabet   = Int
+                | Bool
+                
+                | Var
+                | Pid
+                | Type
+                
+                | If
+                | Else
+                | While
+                | Procedure
+                
+                | Global
+                | Fork
+                | Join
+                
+                -- punctuation
+                | BraceO
+                | BraceC
+                | ParO
+                | ParC
+                | Op
+                | Comma
+                | Semi
+                | Ass
