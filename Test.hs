3f487efc2ab29fdf172d1c40a413a039f24a6306
Tim Kerkhoven <t.kerkhoven@student.utwente.nl>
2016-06-14 11:55:03 +0200

    webpage and more test

--- Test.hs
+++ Test.hs
@@ -4,12 +4,36 @@ import Tokenizer
 import Types
 import Grammar
 import FP_ParserGen         -- Touching this file leaves you at your own devices
+import FPPrac.Trees
 import Debug.Trace
 import System.FilePath
 
-testFib = do 
+prprTestFib = do 
     a <- readFile "test/fib.txt"
     prpr $ 
         parse grammar Program $ 
         toTokenList $ 
+        tokenizer a
+
+roseTestPrime = do
+    a <- readFile "test/fib.txt"
+    showTree $
+        toRoseTree $
+        parse grammar Program $
+        toTokenList $
+        tokenizer a
+
+prprTestPrime = do 
+    a <- readFile "test/prime.txt"
+    prpr $ 
+        parse grammar Program $ 
+        toTokenList $ 
+        tokenizer a
+
+roseTestPrime = do
+    a <- readFile "test/prime.txt"
+    showTree $
+        toRoseTree $
+        parse grammar Program $
+        toTokenList $
         tokenizer a
