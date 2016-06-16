70a253afdf30cf04b947382443c33142a41aaf41
Martijn Verkleij <github@mverkleij.nl>
2016-06-16 11:44:02 +0200

    in between commit

--- Checker.hs
+++ Checker.hs
@@ -13,16 +13,22 @@ checker1 (ASTProgram asts) =
 checker1 (ASTProc str asts ast) =
 checker1 (ASTArg ast1 ast2) = 
 checker1 (ASTBlock asts) =
-checker1 (ASTDecl Bool Alphabet AST (Maybe AST)
-checker1 (ASTIf AST AST (Maybe AST)
-checker1 (ASTWhile AST AST
-checker1 (ASTFork String [AST]
-checker1 (ASTJoin
-checker1 (ASTCall String [AST]
-checker1 (ASTAss AST AST
-checker1 (ASTVar String
-checker1 (ASTInt String
-checker1 (ASTBool String
-checker1 (ASTType String
-checker1 (ASTOp AST String AST
-checker1 (ASTUnary String AST
+checker1 (ASTDecl global {-global :: Bool-} type ast (Nothing) 
+    | global    =
+    | otherwise =
+checker1 (ASTDecl global {-global :: Bool-} type ast1 (Just ast2)
+    | global    =
+    | otherwise =
+checker1 (ASTIf ast1 ast2 (Nothing))) = 
+checker1 (ASTIf ast1 ast2 (Just ast3))) = 
+checker1 (ASTWhile ast ast)) = 
+checker1 (ASTFork str asts)) = 
+checker1 (ASTJoin)) = 
+checker1 (ASTCall str asts) = 
+checker1 (ASTAss ast1 ast2) = 
+checker1 (ASTVar str) = 
+checker1 (ASTInt str) = 
+checker1 (ASTBool str) = 
+checker1 (ASTType str) = 
+checker1 (ASTOp ast1 str ast2) = 
+checker1 (ASTUnary str ast) = 
