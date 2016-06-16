a7bff5528292ab857bc9bb959d5b8631897c4d54
Tim Kerkhoven <t.kerkhoven@student.utwente.nl>
2016-06-16 11:44:58 +0200

    checker stuff

--- Checker.hs
+++ Checker.hs
@@ -9,20 +9,13 @@ checker1 :: AST -> CheckTree
 
 
 
-checker1 (ASTProgram asts) = 
-checker1 (ASTProc str asts ast) =
-checker1 (ASTArg ast1 ast2) = 
-checker1 (ASTBlock asts) =
-checker1 (ASTDecl Bool Alphabet AST (Maybe AST)
-checker1 (ASTIf AST AST (Maybe AST)
-checker1 (ASTWhile AST AST
-checker1 (ASTFork String [AST]
-checker1 (ASTJoin
-checker1 (ASTCall String [AST]
-checker1 (ASTAss AST AST
-checker1 (ASTVar String
-checker1 (ASTInt String
-checker1 (ASTBool String
-checker1 (ASTType String
-checker1 (ASTOp AST String AST
-checker1 (ASTUnary String AST
+checker1 (ASTProgram asts) 
+    = CheckTree ASTProgram [functions] [] []
+        where
+            functions   = $ map checkTree asts
+checker1 (ASTProc str asts ast) 
+    = 
+checker1 (ASTArg ast1 ast2) 
+    = 
+checker1 ast
+    = ast [] [] []
